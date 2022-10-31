3ae0fdb4f4e2955c4fe25f70506bd48979f82261
BradyHebert <89046178+BradyHebert@users.noreply.github.com>
2022-10-31 11:18:39 -0500

    Delete dinnerbox directory

--- dinnerbox/src/main/java/com/genspark/dinnerbox/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.genspark.dinnerbox.repository;
-
-import com.genspark.dinnerbox.entity.ItemEntity;
-import com.genspark.dinnerbox.entity.VendorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
-}
--- dinnerbox/src/main/java/com/genspark/dinnerbox/repository/VendorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.genspark.dinnerbox.repository;
-
-import com.genspark.dinnerbox.entity.VendorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendorRepository extends JpaRepository<VendorEntity, Long> {
-}
